3e49b6b7edbe66bac7e0cde45085c1411a50d5d1
Rene Andre Bedonia Jocsing <rbjocsing129@gmail.com>
2026-01-31 12:48:20 +0800

    fix: make main set core delegate

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,24 @@ import { StoryDownloader } from './modules/StoryDownloader';
 
 /**
  * The Entry Point / Router.
- * Determines which module to load based on the URL path.
- * Saves a bit of performance on a per-page basis.
+ * 1. Identifies the current page path.
+ * 2. Configures the Core Delegate Strategy.
+ * 3. Initializes the specific Feature Module.
  */
 
 const path = window.location.pathname;
 Core.log('router', 'main', `Here at https://www.fanfiction.net${path}`, path);
+Core.setDelegate(path);
 
 // NOTE: The path includes the "/" and omits "https://www.fanfiction.net".
 // If in doubt, check your browser.
 if (path === "/docs/docs.php") {
     DocManager.init();
-} else if (path.includes("/docs/edit.php")) {
+}
+else if (path.includes("/docs/edit.php")) {
     DocEditor.init();
-} else if (path.startsWith("/s/")) {
+}
+else if (path.startsWith("/s/")) {
     // Matches /s/1234569420/1/Story-Title
     StoryReader.init();
     StoryDownloader.init();
