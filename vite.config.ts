97479be2320b0aa3dba8b50d23be38a747d88dd6
Rene Andre Bedonia Jocsing <rbjocsing129@gmail.com>
2026-01-31 11:43:33 +0800

    fix: use cdn instead of bundling external deps

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,23 @@ export default defineConfig({
         author: 'WhiteLicorice',
         match: ['https://www.fanfiction.net/*'],
         grant: ['GM_xmlhttpRequest'],
-        // This plugin automatically handles @require for libraries
+        license: 'GPL-3.0-or-later',
+        // Adding these manually to the Userscript header
+        require: [
+          'https://cdn.jsdelivr.net/npm/jszip@3.10.1/dist/jszip.min.js',
+          'https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js',
+          'https://cdn.jsdelivr.net/npm/turndown@7.2.2/dist/turndown.js'
+        ],
       },
       build: {
         fileName: 'ffn-enhancements.user.js',
+        // This maps the import statements in TS to the Global Variables 
+        // provided by the CDN scripts above.
+        externalGlobals: {
+          'jszip': 'JSZip',
+          'file-saver': 'saveAs',
+          'turndown': 'TurndownService',
+        },
       },
     }),
   ],
