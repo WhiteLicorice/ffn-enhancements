a86c40569fb5dca2af56583fe817533b08571507
Rene Andre Bedonia Jocsing <rbjocsing129@gmail.com>
2026-02-19 06:46:14 +0800

    meta: bump vs for new release

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
       userscript: {
         name: 'FFN Enhancements',
         namespace: 'http://tampermonkey.net/',
-        version: '12.0',
+        version: '12.1',
         author: 'WhiteLicorice',
         match: ['https://www.fanfiction.net/*'],
         'run-at': 'document-start',
