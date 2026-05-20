d8ee8889ec1beaac274141429b00a7f727c28cbf
Rene Andre Bedonia Jocsing <rbjocsing129@gmail.com>
2026-05-20 12:17:03 +0800

    refactor: simplify vite configuration by removing unused critical theme handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,65 +1,42 @@
 import { defineConfig } from 'vite';
-import monkey, { util } from 'vite-plugin-monkey';
-import { buildCriticalThemeCss } from './src/build/criticalThemeCss';
-import {
-  CRITICAL_THEME_STYLE_ID,
-  installCriticalThemePrelude,
-  PRELUDE_ATTRIBUTE,
-  THEME_CACHE_KEY,
-  THEME_STORAGE_KEY,
-  VALID_PRELUDE_THEMES,
-} from './src/prelude/themePrelude';
+import monkey from 'vite-plugin-monkey';
 
-export default defineConfig(async () => {
-  const criticalThemeCss = buildCriticalThemeCss();
-  const criticalThemePrelude = await util.fn2dataUrl(installCriticalThemePrelude, criticalThemeCss, {
-    styleId: CRITICAL_THEME_STYLE_ID,
-    storageKey: THEME_STORAGE_KEY,
-    cacheKey: THEME_CACHE_KEY,
-    preludeAttribute: PRELUDE_ATTRIBUTE,
-    validThemes: VALID_PRELUDE_THEMES,
-  });
-
-  return {
-    plugins: [
-      monkey({
-        entry: 'src/main.ts',
-        userscript: {
-          name: 'FFN Enhancements',
-          namespace: 'http://tampermonkey.net/',
-          version: '15.0',
-          author: 'WhiteLicorice',
-          connect: ['fichub.net', 'archiveofourown.org', 'www.fanfiction.net', 'fanfiction.net'],
-          match: ['https://www.fanfiction.net/*', 'https://archiveofourown.org/*'],
-          'run-at': 'document-start',
-          noframes: true,
-          require: [
-            criticalThemePrelude,
-          ],
-          grant: [
-            'GM_xmlhttpRequest',
-            'GM_getValue',
-            'GM_setValue',
-            'GM_registerMenuCommand',
-            'GM_unregisterMenuCommand',
-            'GM_addValueChangeListener',
-            'GM_deleteValue',
-            'GM_openInTab',
-            'GM_setClipboard',
-          ],
-          license: 'GPL-3.0-or-later',
-          updateURL: 'https://github.com/WhiteLicorice/ffn-enhancements/releases/latest/download/ffn-enhancements.user.js',
-          downloadURL: 'https://github.com/WhiteLicorice/ffn-enhancements/releases/latest/download/ffn-enhancements.user.js',
-        },
-        build: {
-          fileName: 'ffn-enhancements.user.js',
-        },
-      }),
-    ],
-    build: {
-      minify: 'esbuild',
-      cssMinify: true,
-      target: 'es2020',
-    },
-  };
+export default defineConfig({
+  plugins: [
+    monkey({
+      entry: 'src/main.ts',
+      userscript: {
+        name: 'FFN Enhancements',
+        namespace: 'http://tampermonkey.net/',
+        version: '15.0',
+        author: 'WhiteLicorice',
+        connect: ['fichub.net', 'archiveofourown.org', 'www.fanfiction.net', 'fanfiction.net'],
+        match: ['https://www.fanfiction.net/*', 'https://archiveofourown.org/*'],
+        'run-at': 'document-start',
+        noframes: true,
+        grant: [
+          'GM_xmlhttpRequest',
+          'GM_getValue',
+          'GM_setValue',
+          'GM_registerMenuCommand',
+          'GM_unregisterMenuCommand',
+          'GM_addValueChangeListener',
+          'GM_deleteValue',
+          'GM_openInTab',
+          'GM_setClipboard',
+        ],
+        license: 'GPL-3.0-or-later',
+        updateURL: 'https://github.com/WhiteLicorice/ffn-enhancements/releases/latest/download/ffn-enhancements.user.js',
+        downloadURL: 'https://github.com/WhiteLicorice/ffn-enhancements/releases/latest/download/ffn-enhancements.user.js',
+      },
+      build: {
+        fileName: 'ffn-enhancements.user.js',
+      },
+    }),
+  ],
+  build: {
+    minify: 'esbuild',
+    cssMinify: true,
+    target: 'es2020',
+  },
 });
